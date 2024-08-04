7c3e09730531c2436c073d1f19679dfc067da82c
Laptop <hassanabdullah40@gmail.com>
2024-08-05 01:03:08 +0500

    show skeleton on product detail page

--- client/src/pages/Product/ProductDetail.jsx
+++ client/src/pages/Product/ProductDetail.jsx
@@ -8,7 +8,7 @@ import { Button } from "../../components/ui/button";
 import { useMediaQuery } from "react-responsive";
 
 import { useToast } from "../../components/ui/use-toast";
-
+import { Skeleton } from "../../components/ui/skeleton";
 const colors = [
   { name: "black", bgColor: "bg-black" },
   { name: "white", bgColor: "bg-white" },
@@ -25,6 +25,7 @@ const ProductDetail = () => {
   const isSmallScreen = useMediaQuery({ query: "(max-width: 640px)" });
   const [memory, setMemory] = useState(null);
   const [selectedColor, setSelectedColor] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const handleMemory = (value) => {
     setMemory(value);
@@ -58,82 +59,129 @@ const ProductDetail = () => {
   };
 
   useEffect(() => {
-    dispatch(fetchProductByIdAsync(params.id));
+    setLoading(true);
+    const fetchData = async () => {
+      try {
+        await dispatch(fetchProductByIdAsync(params.id));
+      } catch (error) {
+        console.error("Error fetching product:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
   }, [dispatch, params.id]);
 
   return (
-    <>
-      {product && (
-        <Card class=" sm:p-4 py-4 px-2">
-          <div class="flex flex-col lg:flex-row">
-            <div class="w-full lg:w-1/2 p-4 hidden lg:flex">
-              <img
-                src={product.thumbnail}
-                alt="Basic Tee"
-                class="w-full h-auto rounded-lg shadow-lg  "
-              />
+    <div>
+      {loading ? (
+        <div class="grid sm:grid-cols-2 sm:space-x-5 p-5">
+          <div className="hidden sm:inline-block">
+            <Skeleton className="h-screen w-auto rounded-lg" />
+          </div>
+          <div>
+            {/* Title and price placeholders */}
+            <div className="flex justify-between items-center space-y-2">
+              <Skeleton className="h-4 w-[150px]" />
+              <Skeleton className="h-4 w-[50px]" />
             </div>
 
-            <div class="w-full lg:w-1/2 p-4">
-              <div class="flex justify-between items-center">
-                <CardTitle class=" sm:text-xl">{product.title}</CardTitle>
-                <CardTitle class="sm:text-xl ">${product.price}</CardTitle>
-              </div>
+            {/* Color options placeholder */}
+            <div className="flex items-center mt-2 space-x-2">
+              <Skeleton className="h-6 w-6 rounded-full" />
+              <Skeleton className="h-6 w-6 rounded-full" />
+              <Skeleton className="h-6 w-6 rounded-full" />
+            </div>
 
-              <div class="w-full pt-7 pb-2 flex lg:hidden">
+            {/* Memory options placeholder */}
+            <div className="flex items-center space-x-2 mt-3">
+              <Skeleton className="h-4 w-[40px]" />
+              <Skeleton className="h-4 w-[40px]" />
+              <Skeleton className="h-4 w-[40px]" />
+              <Skeleton className="h-4 w-[40px]" />
+            </div>
+
+            {/* Add to cart button placeholder */}
+            <Skeleton className="h-8 w-full rounded-md mt-3" />
+
+            {/* Description placeholder */}
+            <Skeleton className="h-10 w-full rounded-lg mt-3" />
+          </div>
+        </div>
+      ) : (
+        product && (
+          <Card class=" sm:p-4 py-4 px-2">
+            <div class="flex flex-col lg:flex-row">
+              <div class="w-full lg:w-1/2 p-4 hidden lg:flex">
                 <img
-                  // src="https://images.unsplash.com/photo-1611348586804-61bf6c080437?w=300&dpr=2&q=80"
                   src={product.thumbnail}
                   alt="Basic Tee"
                   class="w-full h-auto rounded-lg shadow-lg  "
                 />
               </div>
 
-              <div className="my-6">
-                <span className="font-semibold">Color:</span>
-                <div className="flex items-center mt-2">
-                  {colors.map((color) => (
-                    <span
-                      key={color.name}
-                      className={`w-10 h-10 rounded-full cursor-pointer border-2 ${
-                        color.bgColor
-                      } ${
-                        selectedColor === color.name
-                          ? "border-red-600 w-12 h-12"
-                          : ""
-                      } mr-2`}
-                      onClick={() => handleColorClick(color)}
-                    ></span>
-                  ))}
+              <div class="w-full lg:w-1/2 p-4">
+                <div class="flex justify-between items-center">
+                  <CardTitle class=" sm:text-xl">{product.title}</CardTitle>
+                  <CardTitle class="sm:text-xl ">${product.price}</CardTitle>
                 </div>
-              </div>
-              <div class="my-6">
-                <span class="font-semibold">Memory</span>
-                <div className="flex items-center space-x-2 mt-2">
-                  {["32", "64", "128", "512"].map((value) => (
-                    <Button
-                      key={value}
-                      size={isSmallScreen ? "sm" : "default"}
-                      variant={memory === value ? "default" : "outline"}
-                      onClick={() => handleMemory(value)}
-                    >
-                      {value}
-                    </Button>
-                  ))}
-                </div>
-              </div>
 
-              <Button className="min-w-full" onClick={handleCart}>
-                Add to cart
-              </Button>
-              <div class="mt-3">
-                <p class="">{product.description}</p>
+                <div class="w-full pt-7 pb-2 flex lg:hidden">
+                  <img
+                    // src="https://images.unsplash.com/photo-1611348586804-61bf6c080437?w=300&dpr=2&q=80"
+                    src={product.thumbnail}
+                    alt="Basic Tee"
+                    class="w-full h-auto rounded-lg shadow-lg  "
+                  />
+                </div>
+
+                <div className="my-6">
+                  <span className="font-semibold">Color:</span>
+                  <div className="flex items-center mt-2">
+                    {colors.map((color) => (
+                      <span
+                        key={color.name}
+                        className={`w-10 h-10 rounded-full cursor-pointer border-2 ${
+                          color.bgColor
+                        } ${
+                          selectedColor === color.name
+                            ? "border-red-600 w-12 h-12"
+                            : ""
+                        } mr-2`}
+                        onClick={() => handleColorClick(color)}
+                      ></span>
+                    ))}
+                  </div>
+                </div>
+                <div class="my-6">
+                  <span class="font-semibold">Memory</span>
+                  <div className="flex items-center space-x-2 mt-2">
+                    {["32", "64", "128", "512"].map((value) => (
+                      <Button
+                        key={value}
+                        size={isSmallScreen ? "sm" : "default"}
+                        variant={memory === value ? "default" : "outline"}
+                        onClick={() => handleMemory(value)}
+                      >
+                        {value}
+                      </Button>
+                    ))}
+                  </div>
+                </div>
+
+                <Button className="min-w-full" onClick={handleCart}>
+                  Add to cart
+                </Button>
+                <div class="mt-3">
+                  <p class="">{product.description}</p>
+                </div>
               </div>
             </div>
-          </div>
-        </Card>
+          </Card>
+        )
       )}
-    </>
+    </div>
   );
 };
 
